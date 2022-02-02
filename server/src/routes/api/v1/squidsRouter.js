7676ddac07348af87e7ca3090c356aabce9ccdba
Martin Serna <martin.serna@launchacademy.co>
2022-02-02 17:55:18 -0500

    Paginate squids on server-side, create paging ui

--- server/src/routes/api/v1/squidsRouter.js
+++ server/src/routes/api/v1/squidsRouter.js
@@ -6,10 +6,12 @@ import { nextWrapper } from "../../lib/nextWrapper.js";
 const squidsRouter = new express.Router();
 
 squidsRouter.get(
-  "/",
+  "/:page",
   nextWrapper(async (req, res) => {
-    const squids = await Squid.query();
-    return res.json({ squids });
+    const currentPage = req.params.page;
+    const resultsPerPage = 10;
+    const { results: squids, total } = await Squid.query().page(currentPage - 1, resultsPerPage);
+    return res.json({ squids, currentPage, pageCount: total / resultsPerPage });
   })
 );
 
